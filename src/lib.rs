1004b1a51a44cd93b4853baf09bcb1db6dfc3b68
140bpmdubstep <laineprikol@gmail.com>
2020-07-21 21:04:25 +0400

    Changed architecture

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! # Console (Crossterm) double-buffering library
+//! ![Scheme of working](https://i.imgur.com/K6dZxZy.png)
+
+
 use crossterm::{
     cursor,
     style::Print,
@@ -8,6 +12,7 @@ use std::mem;
 
 #[derive(Clone)]
 pub struct FrameBuffer {
+    current_frame: Vec<char>,
     frame: Vec<char>,
     width: u16,
     height: u16,
@@ -19,27 +24,35 @@ fn do_vecs_match<T: PartialEq>(a: &Vec<T>, b: &Vec<T>) -> bool {
 }
 
 impl FrameBuffer {
-    /// Creates a framebuffer frame
+    /// Creates an empty framebuffer with specified dimensions. Usually it is created at the beginning of the program. But needs to recreate if terminal window is resized
     pub fn create(width: u16, height: u16) -> Self {
         let mut frame = Vec::with_capacity((width * height).into());
+        let mut current_frame = Vec::with_capacity((width * height).into());
+
+        for _ in 0..(width * height) {
+            current_frame.push(' '); // empty symbol...
+        }
+
         for _ in 0..(width * height) {
             frame.push(' '); // empty symbol...
         }
+
         FrameBuffer {
             frame,
+            current_frame,
             width,
             height,
         }
     }
 
     /// Draws a frame in console. Recommended disable cursor. Because it will interfere with enjoyment of the drawing
-    pub fn push_fb(&mut self, mut frame: Vec<char>, stdout: &mut std::io::Stdout) {
-        if !do_vecs_match(&frame, &self.frame) {
-            mem::swap(&mut frame, &mut self.frame);
+    pub fn push_fb(&mut self, stdout: &mut std::io::Stdout) {
+        if !do_vecs_match(&self.frame, &self.current_frame) {
+            mem::swap(&mut self.frame, &mut self.current_frame);
 
             for x in 0..self.width {
                 for y in 0..self.height {
-                    if self.frame[(y * self.width + x) as usize] != frame[(y * self.width + x) as usize]
+                    if self.frame[(y * self.width + x) as usize] != self.current_frame[(y * self.width + x) as usize]
                     {
                         &stdout.execute(cursor::MoveTo(x, y));
                         &stdout.execute(Print(self.frame[(y * self.width + x) as usize]));
@@ -49,7 +62,7 @@ impl FrameBuffer {
         }
     }
 
-    /// Get symbol in framebuffer
+    /// Get symbol in framebuffer in specified coordinates
     pub fn get(&mut self, x: u16, y: u16) -> char {
         self.frame[(y * self.width + x) as usize]
     }
@@ -57,8 +70,8 @@ impl FrameBuffer {
     /// Set string in framebuffer
     pub fn set(&mut self, text: &str, x: u16, y: u16) {
         for (i, v) in text.chars().enumerate() {
-            if (y * self.width + (x + i as u16)) < self.frame.len() as u16 {
-                self.frame[(y * self.width + (x + i as u16)) as usize] = v;
+            if (y * self.width + (x + i as u16)) < self.current_frame.len() as u16 {
+                self.current_frame[(y * self.width + (x + i as u16)) as usize] = v;
             }
         }
     }
